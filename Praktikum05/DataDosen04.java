e5ab6be05fabf194107e0cbd141df95d14344d21
fajareka1 <fajareka.haltec@gmail.com>
2025-03-28 13:27:15 +0700

    second commit

--- Praktikum05/DataDosen04.java
+++ Praktikum05/DataDosen04.java
@@ -61,4 +61,55 @@ public class DataDosen04 {
             dosen[j] = tmp;
         }
     }
+
+    void pencarianDataSequential(String nama) {
+        boolean ditemukan = false;
+        int jumlahDitemukan = 0;
+        for (int i = 0; i < index; i++) {
+            if (dosen[i].nama.equalsIgnoreCase(nama)) {
+                dosen[i].tampil();
+                ditemukan = true;
+                jumlahDitemukan++;
+            }
+        }
+        if (!ditemukan) {
+            System.out.println("Dosen dengan nama " + nama + " tidak ditemukan.");
+        } else if (jumlahDitemukan > 1) {
+            System.out.println("Peringatan: Ditemukan lebih dari satu dosen dengan nama " + nama);
+        }
+    }
+
+
+    void pencarianDataBinary(int usia){
+        sortingAscending(); 
+        int left = 0, right = index - 1;
+        boolean ditemukan = false;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (dosen[mid].usia == usia) {
+                int start = mid, end = mid;
+                while (start > 0 && dosen[start - 1].usia == usia) {
+                    start--;
+                }
+                while (end < index - 1 && dosen[end + 1].usia == usia) {
+                    end++;
+                }
+                for (int i = start; i <= end; i++) {
+                    dosen[i].tampil();
+                }
+                if (end > start) {
+                    System.out.println("Peringatan: Ditemukan lebih dari satu dosen dengan usia " + usia);
+                }
+                ditemukan = true;
+                break;
+            } else if (dosen[mid].usia < usia) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (!ditemukan) {
+            System.out.println("Dosen dengan usia " + usia + " tidak ditemukan.");
+        }
+    }
 }
--- Praktikum05/DosenDemo04.java
+++ Praktikum05/DosenDemo04.java
@@ -7,18 +7,21 @@ public class DosenDemo04 {
         Scanner sc = new Scanner(System.in);
         DataDosen04 listDosen = new DataDosen04();
         int pilihan;
-        
+
         do {
             System.out.println("\nMenu:");
             System.out.println("1. Tambah Data Dosen");
             System.out.println("2. Tampilkan Data Dosen");
             System.out.println("3. Sorting ASC (Termuda ke Tertua)");
             System.out.println("4. Sorting DESC (Tertua ke Termuda)");
-            System.out.println("5. Keluar");
+            System.out.println("5. Pencarian Data berdasarkan Nama");
+            System.out.println("6. Pencarian Data berdasarkan Usia");
+            System.out.println("7. Keluar");
+
             System.out.print("Pilih menu: ");
             pilihan = sc.nextInt();
             sc.nextLine();
-            
+
             switch (pilihan) {
                 case 1:
                     if (listDosen.index < 10) {
@@ -31,7 +34,7 @@ public class DosenDemo04 {
                         System.out.print("Masukkan Usia: ");
                         int usia = sc.nextInt();
                         sc.nextLine();
-                        
+
                         Dosen04 dsn = new Dosen04(kode, nama, ((jk == 'L' || jk == 'l') ? true : false), usia);
                         listDosen.tambahDosen(dsn);
                     } else {
@@ -50,13 +53,25 @@ public class DosenDemo04 {
                     listDosen.tampilDosen();
                     break;
                 case 5:
+                    System.out.print("Masukkan Nama Dosen yang dicari: ");
+                    String cariNama = sc.nextLine();
+                    listDosen.pencarianDataSequential(cariNama);
+                    break;
+                case 6:
+                    listDosen.sortingAscending();
+                    System.out.print("Masukkan Usia Dosen yang dicari: ");
+                    int cariUsia = sc.nextInt();
+                    sc.nextLine();
+                    listDosen.pencarianDataBinary(cariUsia);
+                    break;
+                case 7:
                     System.out.println("Program selesai.");
                     break;
                 default:
                     System.out.println("Pilihan tidak valid!");
             }
         } while (pilihan != 5);
-        
+
         sc.close();
     }
 }
